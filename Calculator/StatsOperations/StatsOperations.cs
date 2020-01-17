daa99ef588f541746c995ed58e4784db9ac3f357
wcmsukwa <wcmsukwa@gmail.com>
2020-01-17 15:23:11 +0200

    Task: adding Median and Mode and Refactoring Rounding

--- Calculator/StatsOperations/StatsOperations.cs
+++ Calculator/StatsOperations/StatsOperations.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-using BasicCalculator;
+using System.Collections.Generic;
 namespace StatsOperations
 {
     public class StatsOperations
@@ -7,16 +7,16 @@ namespace StatsOperations
         
         public static dynamic Mean(dynamic values)
         {
-            Calculator basicCalculator = new Calculator();
-            var sum = basicCalculator.Add(values);
+        
+            var sum = operations.Addition.Sum(values);
             var valueCount = Helpers.Array.Length(values);
-            var result = basicCalculator.Divide(sum, valueCount);
+            var result = operations.Division.Quotient(sum, valueCount);
             return result;
         }
 
         public static dynamic StandardDeviation(dynamic values)
         {
-            Calculator basicCalculator = new Calculator();
+            
             //number of data points
             int numberOfDataPoints = Helpers.Array.Length(values);
             double[] dataPointsArray = new double[numberOfDataPoints];
@@ -26,14 +26,14 @@ namespace StatsOperations
             foreach ( dynamic dataPoint in values)
             {
                 var distanceToMean = mean - dataPoint;
-                var squareDistanceToMean = basicCalculator.Squared(distanceToMean);
+                var squareDistanceToMean = operations.Square.Squared(distanceToMean);
                 dataPointsArray[--numberOfDataPoints] = squareDistanceToMean;
             }
             //Step 3 - Sum the values from Step 2
             //Step 4 - Divide by the number of data points.
             var resultOfStep3and4 = Mean(dataPointsArray);
             //Step 5 - Take the square root.
-            var result = basicCalculator.Squareroot(resultOfStep3and4);
+            var result = operations.SquareRoot.Squareroot(resultOfStep3and4);
             return result;
         }
 
@@ -49,10 +49,65 @@ namespace StatsOperations
 
         public static dynamic ZSquare(dynamic dataPoint, dynamic mean,dynamic standardDeviation)
         {
-            Calculator basicCalculator = new Calculator();
-            var difference = basicCalculator.Subtract(dataPoint,mean);
-            var result = basicCalculator.Divide(difference, standardDeviation);
+            var difference = operations.Subtraction.Subtract(dataPoint,mean);
+            var result = operations.Division.Quotient(difference, standardDeviation);
             return result;
         }
+
+        public static dynamic Median(dynamic values)
+        {
+            var median = 0.0;
+            var valueCount = Helpers.Array.Length(values);
+            if( valueCount % 2 == 0 ) //even number
+            {
+                var step1 = operations.Division.Quotient(valueCount, 2);
+                var step2 = operations.Addition.Sum(step1, 1);
+                var step3 = operations.Addition.Sum(step1, step2);
+                median = operations.Division.Quotient(step3, 2);
+            }
+            else //old number
+            {
+                var valueCountPlusOne = operations.Addition.Sum(valueCount, 1);
+                median = operations.Division.Quotient(valueCountPlusOne, 2);
+            }
+            return median;
+        }
+
+
+        public static dynamic Mode(dynamic values)
+        {
+                int arrayLength = Helpers.Array.Length(values);
+                // Insert all elements in hash 
+                Dictionary<double, int> hp = new Dictionary<double, int>();
+
+                for (int i = 0; i < arrayLength; i++)
+                {
+                    double key = values[i];
+                    if (hp.ContainsKey(key))
+                    {
+                        int freq = hp[key];
+                        freq++;
+                        hp[key] = freq;
+                    }
+                    else
+                        hp.Add(key, 1);
+                }
+
+            // find max frequency. 
+            int min_count = 0;
+            double result = -1;
+
+                foreach (KeyValuePair<double,int> pair in hp)
+                {
+                    if (min_count < pair.Value)
+                    {
+                        result = pair.Key;
+                        min_count = pair.Value;
+                    }
+                }
+                return result;
+            
+        }
+
     }
 }
--- Calculator/StatsOperationsTests/StatsOperationsTests.cs
+++ Calculator/StatsOperationsTests/StatsOperationsTests.cs
@@ -15,5 +15,34 @@ namespace StatsOperations.Tests
             double[] a = { 10.45, 20.5, 10, 20.1, 15.5 };
             Assert.AreEqual( 15.31,StatsOperations.Mean(a));
         }
+
+        [TestMethod()]
+        public void MedianTest()
+        {
+            double[] a = { 10.45, 20.5, 10, 20.1, 15.5 };
+            Assert.AreEqual(3, StatsOperations.Median(a));
+        }
+
+        [TestMethod()]
+        public void StandardDeviationTest()
+        {
+            double[] a = { 10.45, 20.5, 10, 20.1, 15.5 };
+            Assert.AreEqual(4.51, StatsOperations.StandardDeviation(a));
+        }
+
+        [TestMethod()]
+        public void ZSquareTest()
+        {
+            double[] a = { 10.45, 20.5, 10, 20.1, 15.5 };
+            Assert.AreEqual(1.15, StatsOperations.ZSquare(20.5,a));
+        }
+
+
+        [TestMethod()]
+        public void ModeTest()
+        {
+            double[] a = { 15.50, 20.5, 10, 20.1, 15.5 };
+            Assert.AreEqual(15.5, StatsOperations.Mode(a));
+        }
     }
 }
